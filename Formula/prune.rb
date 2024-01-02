8aae7b6467b1d52b8fb02b0895cd2b54fc272af2
nic <nicholas.m.williams@colorado.edu>
2024-01-01 22:32:10 -0700

    making homebrew tap

--- Formula/prune.rb
+++ Formula/prune.rb
@@ -1,6 +1,6 @@
 class Prune < Formula
   desc "A script to uninstall Homebrew packages not accessed since a specified date"
-  homepage "https://github.com/nicknacnic/homebrew-prune"
+  homepage "https://github.com/nicknacnic/Homebrew-Prune"
   url "https://github.com/nicknacnic/Homebrew-Prune/archive/refs/tags/1.0.0.zip"
   sha256 "e34314380f6a246d8a651be8b7250e4f6b45e008e834ca27a1374c085f3ae0be"
 
