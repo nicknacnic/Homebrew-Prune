2c837b4fe03dd0f79ab428d7a4a2b13a46c4388c
nic <nicholas.m.williams@colorado.edu>
2024-01-01 22:13:26 -0700

    update releases/archive taxonomy

--- prune.rb
+++ prune.rb
@@ -1,7 +1,7 @@
 class Prune < Formula
   desc "A script to uninstall Homebrew packages not accessed since a specified date"
   homepage "https://github.com/nicknacnic/Homebrew-Prune"
-  url "https://github.com/nicknacnic/Homebrew-Prune/archive/v1.0.0.tar.gz"
+  url "https://github.com/nicknacnic/Homebrew-Prune/releases/v1.0.0.tar.gz"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
 
   def install
