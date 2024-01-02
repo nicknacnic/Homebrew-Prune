5d10584028aa9bbd9cb917e571976ef49eef8718
nic <nicholas.m.williams@colorado.edu>
2024-01-01 23:12:21 -0700

    update to 1.0.1

--- Formula/prune.rb
+++ Formula/prune.rb
@@ -6,6 +6,10 @@ class Prune < Formula
 
   depends_on "jq"
   
+  homepage "https://github.com/nicknacnic/Homebrew-Prune"
+  url "https://github.com/nicknacnic/Homebrew-Prune/archive/refs/tags/1.0.1.zip"
+  sha256 "49a7228fe6efdaff33cc99f42937dc2736b44e00f200b60e8c1a5f64f2d37a8e"
+
   def install
     bin.install "prune.sh" => "prune"
   end
