939fff44ebe88dd979377478531fd9578751e35d
nic <nicholas.m.williams@colorado.edu>
2024-01-01 22:40:53 -0700

    added error catching to prune.sh

--- prune.sh
+++ prune.sh
@@ -58,6 +58,7 @@ default_date=$(calculate_default_date)
 
 # Initially set compare_date to default_date
 compare_date=$default_date
+prune_count=0
 
 # Help menu
 print_help() {
@@ -316,25 +317,47 @@ while getopts "d:acpthrvg" opt; do
     esac
 done
 
-# Set default_date and compare_date if not set by -d flag
-if [ -z "$compare_date" ]; then
-    default_date=$(calculate_default_date)
-    compare_date=$default_date
-fi
+# Function to parse and compare dates
+parse_and_compare_date() {
+    local last_access_str=$1
+    local last_access_date
 
-# Logging 
-log_message ""
-log_message "Pruned on $current_datetime"
-log_message ""
-log_message "Verbose Information: (1 = yes | 0 = no)"
-log_message ""
-log_message "Test Mode = $test_mode, Process Casks = $process_casks, Process Packages = $process_packages"
-log_message ""
-log_message "Default Date (YYYYMMDD): $default_date"
-log_message "Compare date (YYYYMMDD): $compare_date"
-log_message ""
-log_message "Debug: total_size_kb=$total_size_kb, total_size_cask_kb=$total_size_cask_kb, package_prune_count=$package_prune_count, cask_prune_count=$cask_prune_count"
-log_message ""
+    # Determine if the date string is from the current year or a previous year
+    if [[ "$last_access_str" =~ [0-9]{2}:[0-9]{2} ]]; then
+        # Current year: Format "Mon DD HH:MM"
+        last_access_date=$(date -j -f "%b %d %H:%M %Y" "$last_access_str $(date +%Y)" +"%s")
+    else
+        # Previous year: Format "Mon DD YYYY"
+        last_access_date=$(date -j -f "%b %d %Y" "$last_access_str" +"%s")
+    fi
+
+    if [ "$last_access_date" -lt "$compare_date" ]; then
+        return 0 # Date is before the specified date
+    else
+        return 1 # Date is on or after the specified date
+    fi
+}
+
+# Function to check last access time and (un)install if older than specified date
+check_and_uninstall() {
+    local file_path=$1
+    local package_name=$2
+    if [ -f "$file_path" ]; then
+        # Get last access time of the file
+        local last_access_str=$(ls -lu "$file_path" | awk '{print $6, $7, $8}')
+
+        if parse_and_compare_date "$last_access_str"; then
+            ((prune_count++))
+            if [ "$test_mode" -eq 1 ]; then
+                echo "$package_name would be removed (last accessed on $last_access_str)."
+            else
+                echo "$package_name last accessed on $last_access_str, uninstalling..."
+                brew uninstall "$package_name"
+                echo "$package_name successfully removed."
+            fi
+        fi
+    fi
+}
 
 # Check if no options were provided
 if [ $OPTIND -eq 1 ]; then
