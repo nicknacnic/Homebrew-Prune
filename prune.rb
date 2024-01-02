7b35df0dcf1efc7510ec9cfa7cc7da6c3015b464
nic <nicholas.m.williams@colorado.edu>
2024-01-01 22:05:14 -0700

    updated repo name

--- prune.rb
+++ prune.rb
@@ -1,7 +1,7 @@
 class Prune < Formula
   desc "A script to uninstall Homebrew packages not accessed since before 2022"
   homepage "https://github.com/nicknacnic/Homebrew-Prune"
-  url "https://github.com/nicknacnick/Homebrew-Prune/archive/v1.0.0.tar.gz"
+  url "https://github.com/nicknacnic/Homebrew-Prune/archive/v1.0.0.tar.gz"
   sha256 "779905dadb9e6bf30921cdeeda6ce6bb6a0c16d5"
   license "MIT"
 
