4840cac2c5d02cdf9813b132ebae3419a10e6a04
nic <nicholas.m.williams@colorado.edu>
2024-01-01 21:56:31 -0700

    Updated shasum

--- prune.rb
+++ prune.rb
@@ -2,7 +2,7 @@ class Prune < Formula
   desc "A script to uninstall Homebrew packages not accessed since before 2022"
   homepage "https://github.com/nicknacnic/Homebrew-Prune"
   url "https://github.com/nicknacnick/Homebrew-Prune/archive/v1.0.0.tar.gz"
-  sha256 "29f09ad7e67399708462771be76868e366d9f1ac"
+  sha256 "779905dadb9e6bf30921cdeeda6ce6bb6a0c16d5"
   license "MIT"
 
   def install
