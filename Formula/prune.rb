b8b55492ac6ffd421ed121581c962fa08699e052
nic <nwillau900@gmail.com>
2024-01-03 21:30:45 -0700

    update to v1.1

--- Formula/prune.rb
+++ Formula/prune.rb
@@ -1,7 +1,7 @@
 class Prune < Formula
   desc "A script to uninstall Homebrew packages not accessed since a specified date"
   homepage "https://github.com/nicknacnic/homebrew-prune"
-  url "https://github.com/nicknacnic/Homebrew-Prune/archive/refs/tags/1.0.1.zip"
+  url "https://github.com/nicknacnic/Homebrew-Prune/archive/refs/tags/1.1.zip"
   sha256 "f4b71fb7b69c04edb252b5240f6a8bfc68d14b7aaaf98904c372f3306dd5eb48"
 
   depends_on "jq"
