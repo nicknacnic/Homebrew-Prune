761eb995dcd7775cd5b68cf4062c822117109b67
Layer8 <NWillAU900@gmail.com>
2024-01-03 21:46:56 -0700

    Update prune.rb

--- Formula/prune.rb
+++ Formula/prune.rb
@@ -2,7 +2,7 @@ class Prune < Formula
   desc "A script to uninstall Homebrew packages not accessed since a specified date"
   homepage "https://github.com/nicknacnic/homebrew-prune"
   url "https://github.com/nicknacnic/homebrew-prune/archive/refs/tags/1.1.zip"
-  sha256 "c157aae33ed21a79280628d96cf548cda298bab9092c55818d6f2c3f071b20a7"
+  sha256 "76e1ad9ccd66565dd0b58b30b7d15c9e89873d9e31f5b9f5c5e00ef6f1bf3f8d"
 
   depends_on "jq"
 
